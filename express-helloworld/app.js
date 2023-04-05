87575b900f0fff99cec9668ff135ac3c4beee4d9
CORP\rtacconi <ralph.tacconi@capgemini.com>
2023-04-05 18:15:16 -0300

    stage

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World - Dev!\n');
+  res.send('Hello World - Stage!\n');
 });
 
 app.listen(8080, function () {
